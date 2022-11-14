56c2467674faceeda92a39d3f5cc953b9b36105b
Aleilson Gomes de Cerqueira <aleilson.cerqueira@compasso.com.br>
2022-11-14 11:08:42 -0300

    ci: Add TurboRepo cache on deploy build

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,5 +1,5 @@
 import type { StoryObj, Meta } from '@storybook/react'
-import { Box, BoxProps, Text } from '@ignite-ui/react'
+import { Box, BoxProps, Text } from '@ale-ignite-ui/react'
 
 export default {
   title: 'Surfaces/Box',
